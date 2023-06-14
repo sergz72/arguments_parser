170fc3d40bea9f05faeb74a9d43f21ee61d3e15a
Serhii Zashchelkin <serhii.zashchelkin@connectwise.com>
2023-06-14 17:25:57 +0200

    Updates

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,21 @@
-use std::cell::Cell;
+use std::cell::{Cell, RefCell};
 use std::collections::HashMap;
-use std::env::args;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
 
 pub trait ValueHandler {
     fn parse_value(&self, value: &str) -> Result<(), Error>;
+    fn requires_value(&self) -> bool;
+    fn set_value(&self);
 }
 
 pub struct IntParameter {
-    value: Cell<isize>
+    value: Cell<isize>,
 }
 
 impl IntParameter {
     pub fn new(value: isize) -> IntParameter {
-        IntParameter{value: Cell::new(value)}
+        IntParameter { value: Cell::new(value) }
     }
 
     pub fn get_value(&self) -> isize {
@@ -24,18 +25,81 @@ impl IntParameter {
 
 impl ValueHandler for IntParameter {
     fn parse_value(&self, value: &str) -> Result<(), Error> {
-        self.value.set(isize::from_str(value).map_err(|e|Error::new(ErrorKind::InvalidInput, e))?);
+        self.value.set(isize::from_str(value).map_err(|e| Error::new(ErrorKind::InvalidInput, e))?);
         Ok(())
+    }
+
+    fn requires_value(&self) -> bool {
+        return true;
+    }
+
+    fn set_value(&self) {
+    }
+}
+
+pub struct StringParameter {
+    value: RefCell<String>,
+}
+
+impl StringParameter {
+    pub fn new(value: &str) -> StringParameter {
+        StringParameter { value: RefCell::new(value.to_string()) }
+    }
+
+    pub fn get_value(&self) -> String {
+        self.value.borrow().clone()
+    }
+}
+
+impl ValueHandler for StringParameter {
+    fn parse_value(&self, value: &str) -> Result<(), Error> {
+        *self.value.borrow_mut() = value.to_string();
+        Ok(())
+    }
+
+    fn requires_value(&self) -> bool {
+        return true;
+    }
+
+    fn set_value(&self) {
+    }
+}
+
+pub struct BoolParameter {
+    value: Cell<bool>,
+}
+
+impl BoolParameter {
+    pub fn new() -> BoolParameter {
+        BoolParameter { value: Cell::new(false) }
+    }
+
+    pub fn get_value(&self) -> bool {
+        self.value.get()
+    }
+}
+
+impl ValueHandler for BoolParameter {
+    fn parse_value(&self, _value: &str) -> Result<(), Error> {
+        Err(Error::new(ErrorKind::Unsupported, "should not be called"))
+    }
+
+    fn requires_value(&self) -> bool {
+        return false;
+    }
+
+    fn set_value(&self) {
+        self.value.set(true);
     }
 }
 
 pub struct SizeParameter {
-    value: Cell<isize>
+    value: Cell<isize>,
 }
 
 impl SizeParameter {
     pub fn new(value: isize) -> SizeParameter {
-        SizeParameter{value: Cell::new(value)}
+        SizeParameter { value: Cell::new(value) }
     }
 
     pub fn get_value(&self) -> isize {
@@ -46,20 +110,27 @@ impl SizeParameter {
 impl ValueHandler for SizeParameter {
     fn parse_value(&self, value: &str) -> Result<(), Error> {
         let multiplier = match value.chars().last().ok_or(Error::new(ErrorKind::InvalidInput, "invalid size parameter"))? {
-            'M' => 1024*1024,
+            'M' => 1024 * 1024,
             'K' => 1024,
-            'G' => 1024*1024*1024,
+            'G' => 1024 * 1024 * 1024,
             _ => 1
         };
         let size = if multiplier == 1 {
-            isize::from_str(value).map_err(|e|Error::new(ErrorKind::InvalidInput, e))?
+            isize::from_str(value).map_err(|e| Error::new(ErrorKind::InvalidInput, e))?
         } else {
             let mut chars = value.chars();
             chars.next_back();
-            isize::from_str(chars.as_str()).map_err(|e|Error::new(ErrorKind::InvalidInput, e))?
+            isize::from_str(chars.as_str()).map_err(|e| Error::new(ErrorKind::InvalidInput, e))?
         };
         self.value.set(size * multiplier);
         Ok(())
+    }
+
+    fn requires_value(&self) -> bool {
+        return true;
+    }
+
+    fn set_value(&self) {
     }
 }
 
@@ -68,12 +139,12 @@ pub struct Switch<'a> {
     name: String,
     switch: Option<char>,
     ext_switch: Option<String>,
-    handler: &'a dyn ValueHandler
+    handler: &'a dyn ValueHandler,
 }
 
 impl<'a> Switch<'a> {
     pub fn new(name: &str, switch: Option<char>, ext_switch: Option<String>, handler: &'a dyn ValueHandler) -> Switch<'a> {
-        Switch{
+        Switch {
             name: name.to_string(),
             switch,
             ext_switch,
@@ -83,11 +154,20 @@ impl<'a> Switch<'a> {
 
     fn to_string(&self) -> String {
         let mut result = "".to_string();
-        if let Some(sw) = self.switch {
-            result.push_str(format!(" -{} {}", sw, self.name).as_str());
-        }
-        if let Some(sw) = &self.ext_switch {
-            result.push_str(format!(" --{} {}", sw, self.name).as_str());
+        if self.handler.requires_value() {
+            if let Some(sw) = self.switch {
+                result.push_str(format!(" -{} {}", sw, self.name).as_str());
+            }
+            if let Some(sw) = &self.ext_switch {
+                result.push_str(format!(" --{} {}", sw, self.name).as_str());
+            }
+        } else {
+            if let Some(sw) = self.switch {
+                result.push_str(format!(" -{}", sw).as_str());
+            }
+            if let Some(sw) = &self.ext_switch {
+                result.push_str(format!(" --{}", sw).as_str());
+            }
         }
         if result.len() == 0 {
             return self.name.clone();
@@ -99,13 +179,21 @@ impl<'a> Switch<'a> {
         self.handler.parse_value(value)?;
         Ok(())
     }
+
+    fn requires_value(&self) -> bool {
+        self.handler.requires_value()
+    }
+
+    fn set_value(&self) {
+        self.handler.set_value()
+    }
 }
 
 pub struct Arguments<'a> {
     program_name: String,
     switch_map: HashMap<char, Switch<'a>>,
     ext_switch_map: HashMap<String, Switch<'a>>,
-    other_arguments: Vec<String>
+    other_arguments: Vec<String>,
 }
 
 impl<'a> Arguments<'a> {
@@ -120,11 +208,11 @@ impl<'a> Arguments<'a> {
                 ext_switch_map.insert(sw.clone(), switch.clone());
             }
         }
-        Arguments{
+        Arguments {
             program_name: program_name.to_string(),
             switch_map,
             ext_switch_map,
-            other_arguments: Vec::new()
+            other_arguments: Vec::new(),
         }
     }
 
@@ -141,7 +229,7 @@ impl<'a> Arguments<'a> {
     }
 
     pub fn build(&mut self, args: Vec<String>) -> Result<(), Error> {
-        let mut current_parameter: Option<&mut Switch> = None;
+        let mut current_parameter: Option<&Switch> = None;
         for arg in args {
             if let Some(p) = current_parameter {
                 p.parse_value(arg.as_str())?;
@@ -152,8 +240,12 @@ impl<'a> Arguments<'a> {
                         if arg.len() == 2 {
                             return Err(Error::new(ErrorKind::InvalidInput, "invalid ext_switch"));
                         }
-                        if let Some(p) = self.ext_switch_map.get_mut(&arg.chars().skip(2).collect::<String>()) {
-                            current_parameter = Some(p);
+                        if let Some(p) = self.ext_switch_map.get(&arg.chars().skip(2).collect::<String>()) {
+                            if p.requires_value() {
+                                current_parameter = Some(p);
+                            } else {
+                                p.set_value();
+                            }
                         } else {
                             return Err(Error::new(ErrorKind::InvalidInput, "unknown ext switch"));
                         }
@@ -161,8 +253,12 @@ impl<'a> Arguments<'a> {
                         if arg.len() != 2 {
                             return Err(Error::new(ErrorKind::InvalidInput, "invalid switch"));
                         }
-                        if let Some(p) = self.switch_map.get_mut(&arg.chars().skip(1).next().unwrap()) {
-                            current_parameter = Some(p);
+                        if let Some(p) = self.switch_map.get(&arg.chars().skip(1).next().unwrap()) {
+                            if p.requires_value() {
+                                current_parameter = Some(p);
+                            } else {
+                                p.set_value();
+                            }
                         } else {
                             return Err(Error::new(ErrorKind::InvalidInput, "unknown switch"));
                         }
@@ -176,5 +272,38 @@ impl<'a> Arguments<'a> {
             return Err(Error::new(ErrorKind::InvalidInput, "switch value expected"));
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{Arguments, BoolParameter, IntParameter, SizeParameter, StringParameter, Switch};
+
+    #[test]
+    fn test_arguments_parser() {
+        let port_parameter = IntParameter::new(6379);
+        let max_memory_parameter = SizeParameter::new(1024 * 1024 * 1024);//1G
+        let threads_parameter = IntParameter::new(4);
+        let verbose_parameter = BoolParameter::new();
+        let string_parameter = StringParameter::new("init");
+        let switches = [
+            Switch::new("port", Some('p'), None, &port_parameter),
+            Switch::new("maximum_memory", Some('m'), None, &max_memory_parameter),
+            Switch::new("threads", Some('t'), None, &threads_parameter),
+            Switch::new("verbose", Some('v'), None, &verbose_parameter),
+            Switch::new("test", Some('s'), None, &string_parameter),
+        ];
+        let mut arguments = Arguments::new("cache", &switches);
+        assert!(arguments.build(vec![
+            "-p".to_string(), "3333".to_string(),
+            "-m".to_string(), "1M".to_string(),
+            "-t".to_string(), "12".to_string(),
+            "-v".to_string(),
+            "-s".to_string(), "test".to_string()]).is_ok());
+        assert_eq!(3333, port_parameter.get_value());
+        assert_eq!(1024 * 1024, max_memory_parameter.get_value());
+        assert_eq!(12, threads_parameter.get_value());
+        assert_eq!(true, verbose_parameter.get_value());
+        assert_eq!("test", string_parameter.get_value());
     }
 }
